90b27872d7efa74061539f9e5421bab274325d77
JuanbaCallander <callander.bautista@gmail.com>
2022-06-29 19:44:57 -0300

    crud con modelo cvd

--- MiPrimerProyectoApp/urls.py
+++ MiPrimerProyectoApp/urls.py
@@ -6,12 +6,18 @@ from .views import *
 urlpatterns = [
  
     
-    path('tablafamiliares/', tabla_familiares, name= 'familiares'),
+    path('familiares/', familiares, name= 'familiares'),
     path('crear_familiar/', crear_familiar, name= 'crear-familiar'),
     path('buscar_familiar/', buscar_familiar, name='buscar-familiar'),
     path('eliminar_familiar/<familiar_id>', eliminar_familiar, name='eliminar-familiar'),
     path('editar_familiar/<familiar_id>',
          editar_familiar, name='editar-familiar'),
+    
+    path('familiares/list', FamiliarList.as_view(), name='familiares_list'),
+    path(r'^(?P<pk>\d+)$', FamiliarDetail.as_view(), name='familiares_detail'),
+    path(r'^nuevo$', FamiliarCreate.as_view(), name='familiares_create'),
+    path(r'^editar/(?P<pk>\d+)$', FamiliarUpdate.as_view(), name='familiares_update'),
+    path(r'^eliminar(?P<pk>\d+)$', FamiliarDelete.as_view(), name='familiares_delate'),
 
 ]
 
--- MiPrimerProyectoApp/views.py
+++ MiPrimerProyectoApp/views.py
@@ -3,9 +3,16 @@ from sqlite3 import Cursor
 
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from MiPrimerProyectoApp.apps import MiprimerproyectoappConfig
 
 from MiPrimerProyectoApp.models import * 
 from .forms import FamiliarFormulario
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 
 def crear_familiar(request):
@@ -85,11 +92,42 @@ def buscar_familiar(request):
     
         return render(request, 'MiPrimerProyectoApp/busqueda_familiar.html', {'familiares': familiares})
 
-def tabla_familiares(request):
+def familiares(request):
     
     data = Familiar.objects.all()
     
     return render(request, 'MiPrimerProyectoApp/familiares.html', {'data': data})
+
+class FamiliarList(ListView):
+    
+    model = Familiar
+    template_name = 'MiPrimerProyectoApp/familiares_list.html'
+    
+
+class FamiliarDetail(DetailView):
+    
+    model = Familiar
+    template_name = 'MiPrimerProyectoApp/familiares_detail.html'
+    
+class FamiliarCreate(CreateView):
+    
+    model = Familiar
+    success_url = '/coderapp/familiares/list'
+    fields = ['nombre', 'apellido', 'edad', 'fecha_nacimiento']
+    
+
+class FamiliarUpdate(UpdateView):
+
+    model = Familiar
+    success_url = '/coderapp/familiares/list'
+    fields = ['nombre', 'apellido', 'edad', 'fecha_nacimiento']
+    
+
+class FamiliarDelete(DeleteView):
+
+    model = Familiar
+    success_url = '/coderapp/familiares/list'
+    fields = ['nombre', 'apellido', 'edad', 'fecha_nacimiento']
 
 
 def inicio(request):
